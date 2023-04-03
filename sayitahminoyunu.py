9670400c3dcd76bff580d0c09bddf7a6132ba2c9
Furkan Özay <48617418+furkannozay@users.noreply.github.com>
2023-04-03 18:55:03 +0300

    Add files via upload

---
+++ sayitahminoyunu.py
@@ -0,0 +1,18 @@
+﻿import random
+random.seed()   #Prepare random number generator
+
+tutulanSayi = int(random.random() * 50)
+tahminSayisi = 5
+while True:    #This simulates a Do Loop
+    print("bir tahmin giriniz")
+    girilenSayi = int(input())
+    if girilenSayi == tutulanSayi:
+        print("Tebrikler bildiniz" + str(tutulanSayi))
+    else:
+        if girilenSayi < tutulanSayi:
+            print("Daha büyük bir sayı girin")
+        else:
+            print("Daha küçük bir sayı girin")
+    tahminSayisi = tahminSayisi - 1
+    if not(tahminSayisi > 0): break   #Exit loop
+print("Tahmin hakkınız kalmadı :(")
